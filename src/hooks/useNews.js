351c8d1992e09faa80dc02adc65bdd4dda0e15be
Mousoom Mudoi <mousoom44@gmail.com>
2023-02-13 00:30:25 +0530

    changes

--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -2,11 +2,14 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 
 const getPostByLanguage = async language => {
-  const { data } = await axios.get(
-    // `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`,
-    `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=English&date=baarta`,
-    { headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
-  )
+  const { data } = await axios
+    .get(
+      `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`
+    )
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
   const newsData = JSON.parse(data.response)
   return newsData
 }
