a438b3efbf073d95f02e511454bf2742e7ca9dc9
Mousoom Mudoi <mousoom44@gmail.com>
2023-02-12 23:45:25 +0530

    changing api

--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const getPostByLanguage = async language => {
   const { data } = await axios.get(
-    `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`,
+    // `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`,
+    `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=English&date=baarta`,
     { headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' } }
   )
   const newsData = JSON.parse(data.response)
