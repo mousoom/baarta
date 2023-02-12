65e741c066ce9b5cb4f49ce00f2ababe3cff35ce
Mousoom Mudoi <mousoom44@gmail.com>
2023-02-12 20:12:32 +0530

    cors error check

--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const getPostByLanguage = async language => {
   const { data } = await axios.get(
-    `http://ec2-18-193-117-173.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`
+    `http://ec2-18-193-117-173.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`,
+    { headers: { 'Access-Control-Allow-Origin': '*' } }
   )
   const newsData = JSON.parse(data.response)
   return newsData
