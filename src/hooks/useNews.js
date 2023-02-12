fb82c57b0bf98f19ad0357b2c9dedeb8e60a9357
Mousoom Mudoi <mousoom44@gmail.com>
2023-02-13 00:54:30 +0530

    changed headers

--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -2,13 +2,16 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 
 const getPostByLanguage = async language => {
+  let headersList = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
+
   const { data } = await axios
     .get(
       `https://ec2-3-71-35-71.eu-central-1.compute.amazonaws.com/api/posts/getArticles/?language=${language}&date=baarta`,
       {
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: headersList,
       }
     )
     .catch(function (error) {
